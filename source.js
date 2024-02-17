7b04cb45a098670da081a7e52e827baafb1b11ed
Davd <122733045+DMSchell@users.noreply.github.com>
2024-02-17 16:25:46 -0500

    fixing dragging

--- source.js
+++ source.js
@@ -1,15 +1,14 @@
 document.getElementById('NoteMaker').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
-        console.log('Text box value:', event.target.value);
         //make a new note
         var newNoteDragger = document.createElement('div');
-        var newNote = document.createElement('div');
-        newNote.className = 'new-note';
         newNoteDragger.className = 'note-dragger'
-        newNote.textContent = event.target.value;
         newNoteDragger.textContent = ' ';
         document.getElementById('container').appendChild(newNoteDragger);
+        var newNote = document.createElement('div');
+        newNote.className = 'new-note';
+        newNote.textContent = event.target.value;
         newNoteDragger.appendChild(newNote)
         //clear the text box
         event.target.value = '';
